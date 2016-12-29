e0b0bb5fa1e7d7491f62651128ed002b726d4a3b
bassment <anton.perebyinis@gmail.com>
2016-12-29 20:29:00 +0200

    Add fields to scheme and migration

--- lib/baby_store/product.ex
+++ lib/baby_store/product.ex
@@ -2,14 +2,19 @@ defmodule BabyStore.Product do
   use Ecto.Schema
 
   schema "products" do
-    field :title, :string
-    field :price, :integer
-    field :image, :string
+    field :type,      :string
+    field :category,  :string
+    field :title,     :string
+    field :price,     :integer
+    field :image,     :string
+    field :available, :boolean
+    field :createdBy, :string
+    field :createAt,  :utc_datetime
   end
 
   def changeset(product, params \\ %{}) do
     product
-    |> Ecto.Changeset.cast(params, [:title, :price, :image])
+    |> Ecto.Changeset.cast(params, [:type, :category, :title, :price, :image, :available, :createdBy, :createdAt])
     |> Ecto.Changeset.validate_required([:title, :price])
   end
 end
--- priv/repo/migrations/20161221184926_create_product.exs
+++ priv/repo/migrations/20161221184926_create_product.exs
@@ -3,9 +3,14 @@ defmodule BabyStore.Repo.Migrations.CreateProduct do
 
   def change do
     create table(:products) do
-      add :title, :string
-      add :price, :integer
-      add :image, :string
+      add :type,      :string
+      add :category,  :string
+      add :title,     :string
+      add :price,     :integer
+      add :image,     :string
+      add :available, :boolean
+      add :createdBy, :string
+      add :createAt,  :utc_datetime
     end
   end
 end
